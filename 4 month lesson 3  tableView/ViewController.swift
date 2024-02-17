ad919a42fcc010d62d770e60e769babdf353e997
OmurbekovTemirlan <Japarhanovaldik@gmail.com>
2024-02-17 17:00:04 +0600

    Initial Commit

---
+++ 4 month lesson 3  tableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  4 month lesson 3  tableView
+//
+//  Created by Apple on 17.2.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
